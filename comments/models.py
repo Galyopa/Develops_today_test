6b2a70e03fe80cd43ac925ba155f1db624204f80
Galyopa <galyopa@gmail.com>
2021-03-13 13:47:44 +0200

    Create models for News and Comments

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from news.models import News
+
+
+class Comment(models.Model):
+    news = models.ForeignKey(News, on_delete=models.CASCADE)
+    author_name = models.CharField(max_length=64)
+    content = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-creation_date']
+
+    def __str__(self):
+        return self.author_name
--- news/models.py
+++ news/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=140)
+    link = models.URLField(max_length=200)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    amount_of_upvotes = models.IntegerField(default=0)
+    author_name = models.CharField(max_length=64)
+
+    class Meta:
+        ordering = ['-creation_date']
+
+    def __str__(self):
+        return self.title
