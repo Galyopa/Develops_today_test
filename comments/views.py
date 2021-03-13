c573f02d710045bc05d154d6e840123da2337dc1
Galyopa <galyopa@gmail.com>
2021-03-13 16:40:28 +0200

    Create CRUD for Comments

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from comments.models import Comment
+from comments.serializer import CommentSerializer
+
+
+class CommentsList(generics.ListCreateAPIView):
+    """
+    List all comments, or create a new.
+    """
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class CommentsListFilteredByNews(generics.ListCreateAPIView):
+    """
+    List all comments filtered by news, or create a new.
+    """
+
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        news = self.kwargs['pk']
+        return Comment.objects.all().filter(news=news)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update or delete a comment instance.
+    """
+
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
